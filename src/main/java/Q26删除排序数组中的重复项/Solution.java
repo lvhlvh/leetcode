d0d33eb2b7b7cec52ff7a8e02fe0005fd329556d
lvhlvh <riviewho@gmail.com>
2020-06-24 10:17:37 +0800

    10:38:12 20-06-17

--- src/main/java/Q26删除排序数组中的重复项/Solution.java
+++ src/main/java/Q26删除排序数组中的重复项/Solution.java
@@ -1,5 +1,11 @@
 package Q26删除排序数组中的重复项;
 
+/**
+ * 条件是 nums[j] != nums[j - 1] 时的分析:
+ *
+ * - 如果数组中不存在重复元素, 则i,j始终保持一致, OK
+ * - 如果数组中存在重复元素，则j - i >= 1, 还是OK
+ */
 public class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
@@ -8,7 +14,8 @@ public class Solution {
         int i, j;
         i = j = 1;
         for (; j < nums.length; ++j) {
-            if (nums[j] != nums[j - 1]) nums[i++] = nums[j];
+            if (nums[j] != nums[i - 1]) // 条件也可以是nums[j] != nums[j - 1]
+                nums[i++] = nums[j];
         }
 
         return i;
