019db69712b7119d0a868db3feda05fd37986250
lvhao <riviewho@gmail.com>
2024-02-14 10:38:23 +0800

    docs: Q876链表的中间节点

--- src/main/java/Q876链表的中间节点/Solution.java
+++ src/main/java/Q876链表的中间节点/Solution.java
@@ -14,6 +14,12 @@ public class Solution {
   public ListNode middleNode(ListNode head) {
     ListNode fast, slow;
     fast = slow = head;
+    // 关于while条件:
+    // 思考方式1:
+    // fast.next != null控制奇数个节点链表的停止条件
+    // fast != null控制偶数个节点链表的停止条件
+    // 思考方式2:
+    // 要想快指针能往后移动两位, 必须fast!=null且fast.next!=null
     while (fast != null && fast.next != null) {
       fast = fast.next.next;
       slow = slow.next;
